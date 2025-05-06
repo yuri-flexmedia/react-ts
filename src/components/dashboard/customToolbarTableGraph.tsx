823ed9e88cc0892172bfe639d05fd9865c33a7dc
Yuri Santana <yuri.sa@flexmedia.com.br>
2025-05-06 10:32:11 -0300

    conclusao tabela e grafico

--- src/components/dashboard/customToolbarTableGraph.tsx
+++ src/components/dashboard/customToolbarTableGraph.tsx
@@ -1,3 +1,28 @@
+import { Tooltip } from "@mui/material";
+import { Toolbar, ToolbarButton } from "@mui/x-data-grid";
+import React from "react";
+import { PieChart } from "lucide-react";
+
+// type ToolbarProps = {
+//   showGraph: boolean;
+// };
+
 export default function CustomToolbarTableGraph() {
-  return <div>CustomToolbarTableGraph</div>;
+  // { showGraph }: ToolbarProps
+  const [showToolbarGraph, setShowGraph] = React.useState(false);
+
+  return (
+    <Toolbar>
+      <Tooltip title="Exibir Gráfico">
+        <ToolbarButton
+          id="show-graph-trigger"
+          aria-controls="show-graph"
+          aria-haspopup="false"
+          onClick={() => setShowGraph(!showToolbarGraph)}
+        >
+          <PieChart fontSize="small" />
+        </ToolbarButton>
+      </Tooltip>
+    </Toolbar>
+  );
 }
--- src/components/dashboard/dataTableGraph.tsx
+++ src/components/dashboard/dataTableGraph.tsx
@@ -1,5 +1,9 @@
-import { Paper } from "@mui/material";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { Card, CardContent, Tooltip, Typography } from "@mui/material";
+import { DataGrid, GridColDef, Toolbar, ToolbarButton } from "@mui/x-data-grid";
+import { pieArcLabelClasses, PieChart } from "@mui/x-charts";
+import { InsertChart, InsertChartOutlined } from "@mui/icons-material";
+import React from "react";
+import { defaultizeValueFormatter } from "@mui/x-charts/internals";
 
 const columns: GridColDef[] = [
   //   { field: "id", headerName: "ID", width: 130,  },
@@ -9,29 +13,109 @@ const columns: GridColDef[] = [
 ];
 
 const rows = [
-  { id: 1, title: "Opa", label: "MARCA MARCADA", play_count: 4444 },
+  { id: 1, title: "flexDS", label: "Apresentação FlexDS", play_count: 4717 },
+  { id: 2, title: "lancamento web", label: "Lançamento Web", play_count: 3474 },
+  { id: 3, title: "promocao daoca", label: "Promoção Daoca", play_count: 2862 },
+  {
+    id: 4,
+    title: "apresentacao plaxis",
+    label: "Apresentação Plaxis",
+    play_count: 2333,
+  },
+];
+
+const graphData = [
+  {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    arcLabel: (item: any) => `${item.value}`,
+    arcLabelMinAngle: 35,
+    arcLabelRadius: "60%",
+    defaultizeValueFormatter,
+    data: [
+      { id: 1, label: "Apresentação FlexDS", value: 4717 },
+      { id: 2, label: "Lançamento Web", value: 3474 },
+      { id: 3, label: "Promoção Daoca", value: 2862 },
+      { id: 4, label: "Apresentação Plaxis", value: 2333 },
+    ],
+  },
 ];
 
 const paginationModel = { page: 0, pageSize: 5 };
 
 export default function DataTableGraph() {
+  const [showToolbarGraph, setShowGraph] = React.useState(false);
+  function CustomToolbarTableGraph() {
+    return (
+      <Toolbar
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: 16,
+            fontWeight: 500,
+          }}
+        >
+          Videos mais assistidos
+        </Typography>
+        <Tooltip title="Exibir Gráfico">
+          <ToolbarButton
+            id="show-graph-trigger"
+            aria-controls="show-graph"
+            aria-haspopup="false"
+            onClick={() => setShowGraph(!showToolbarGraph)}
+          >
+            {showToolbarGraph ? (
+              <InsertChart fontSize="small" />
+            ) : (
+              <InsertChartOutlined fontSize="small" />
+            )}
+          </ToolbarButton>
+        </Tooltip>
+      </Toolbar>
+    );
+  }
+
   return (
-    <Paper
+    <Card
       sx={{
-        height: 400,
-        maxWidth: "500px",
+        // height: 400,
+        maxWidth: !showToolbarGraph ? "500px" : "100%",
         borderRadius: 2,
       }}
     >
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{ pagination: { paginationModel } }}
-        pageSizeOptions={[5, 10]}
-        // checkboxSelection
-        sx={{ border: 0, borderRadius: 2 }}
-        showToolbar
-      />
-    </Paper>
+      <CardContent
+        sx={{
+          display: "flex",
+        }}
+      >
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{ pagination: { paginationModel } }}
+          pageSizeOptions={[5, 10]}
+          // checkboxSelection
+          sx={{ border: 0, borderRadius: 2 }}
+          showToolbar
+          slots={{ toolbar: CustomToolbarTableGraph }}
+        />
+
+        {showToolbarGraph ? (
+          <PieChart
+            series={graphData}
+            width={200}
+            sx={{
+              [`& .${pieArcLabelClasses.root}`]: {
+                fontWeight: "bold",
+                fontFamily: "Segoe UI",
+              },
+            }}
+            hideLegend
+          />
+        ) : null}
+      </CardContent>
+    </Card>
   );
 }
